ebddd997f9d69f7657fe0a37cb571e2f3f49dc85
Isabel <Isabel@1x-vl906-128-119-236-154.wireless.umass.edu>
2015-04-30 15:20:03 -0400

    new book

--- js/script1.js
+++ js/script1.js
@@ -83,7 +83,7 @@ var lesson6 = {
 
     // prepare loader and load the model
     var oLoader = new THREE.OBJLoader();
-    oLoader.load('book.obj', function(object, materials) {
+    oLoader.load('obj/Book2.obj', function(object, materials) {
 
       // var material = new THREE.MeshFaceMaterial(materials);
       var material2 = new THREE.MeshLambertMaterial({ color: 0xa65e00 });
